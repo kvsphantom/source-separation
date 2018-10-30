c5767da0fac193491e221abf8963dea33a6c0dbc
kvsphantom <kvsphantom@gmail.com>
2018-10-30 13:10:46 +0100

    Includes framecutter, dataset splitter and program for retraining resnet

---
+++ data_split.py
@@ -0,0 +1,52 @@
+"""
+Created on 5:55 PM
+
+@author: kvshenoy
+"""
+
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+from shutil import copyfile
+
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.umask(0) #To mask the permission restrictions on new files/directories being create
+        os.makedirs(path,0o755) # setting permissions for the folder
+
+classes=['cello','clarinet','erhu','flute','trumpet','tuba','violin','xylophone']
+input_dir='/home/kvshenoy/project/dataset/juan/fps0.01'
+split_dir='/home/kvshenoy/project/dataset/juan/dummy'
+
+X=[]
+y=[]
+for path, subdirs, files in os.walk(input_dir):
+    for name in files:
+        #file_list.append(name)
+        #input_list.append(os.path.join(path, name))
+        id= os.path.join(path, name)
+        classname=classes.index(os.path.basename(path))
+        X.append(id)
+        y.append(classname)
+
+X=np.array(X)
+y=np.array(y)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.25, random_state=0)
+np.save('./X_train',X_train)
+np.save('./y_train',y_train)
+np.save('./X_val',X_val)
+np.save('./y_val',y_val)
+
+#X_train=np.load('X_train.npy')
+#X_val=np.load('X_val.npy')
+for x in X_train:
+    category=os.path.basename(os.path.dirname(x))
+    save_to_path=os.path.join(split_dir, 'train', category, os.path.basename(x))
+    create_folder(os.path.dirname(save_to_path))
+    copyfile(x,save_to_path)
+for x in X_val:
+    category = os.path.basename(os.path.dirname(x))
+    save_to_path = os.path.join(split_dir, 'val', category, os.path.basename(x))
+    create_folder(os.path.dirname(save_to_path))
+    copyfile(x, save_to_path)
---
+++ video.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 23 17:36:25 2018
+
+@author: kvshenoy
+"""
+
+import os
+import numpy as np
+import cv2
+import math
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.umask(0) #To mask the permission restrictions on new files/directories being create
+        os.makedirs(path,0o755) # setting permissions for the folder
+
+def extract_frames(src_path,dest_path,req_fps,start_times,stop_times,video_metric_log,empty_frame_log):
+    cap = cv2.VideoCapture(src_path)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    real_fps = cap.get(cv2.CAP_PROP_FPS)
+    segment = real_fps/float(req_fps)
+
+    with open(video_metric_log, "a") as video_metric_log_file:
+        video_metric_log_file.write(os.path.basename(src_path)+","+str(cap.get(cv2.CAP_PROP_FPS))+"\n")
+
+    for index in range(len(start_times)):
+        start = start_times[index] * 1000 +500  # in milliseconds
+        stop = stop_times[index] * 1000  -500# in milliseconds
+
+        global_frame_number =math.ceil(start*real_fps/float(1000))
+        sample_pos = 0
+        while cap.isOpened() and cap.get(cv2.CAP_PROP_POS_MSEC) <= stop:
+            selected_sample_number = round((segment * sample_pos) + segment / float(2))  # mid sample from each segment is selected
+            sample_pos += 1
+            cap.set(cv2.CAP_PROP_POS_FRAMES, global_frame_number +selected_sample_number - 1)
+            ret, image = cap.read()
+            if (ret != True and (global_frame_number+selected_sample_number)<total_frames):
+                with open(empty_frame_log, "a") as empty_frame_log_file:
+                    empty_frame_log_file.write(os.path.basename(src_path) + "," +str(int(global_frame_number+selected_sample_number))+"\n")
+                continue
+            basename = os.path.join(dest_path, os.path.basename(src_path)[:-4])
+            filename = os.path.join(dest_path, basename + "_" + str(int(global_frame_number+selected_sample_number)) + ".png")
+            if image is not None:
+                resized_image = cv2.resize(image, (224, 224))  # Resize each of the frames to 224X224
+                cv2.imwrite(filename, resized_image)
+            if (ret != True):
+                break
+    cap.release()
+    #cv2.destroyAllWindows()
+
+fps=0.01 #setting frames per second that we need to sample from each of the video
+classes={'cello','clarinet','erhu','flute','trumpet','tuba','violin','xylophone'}
+input_dir = '/home/kvshenoy/project/dataset/juan/original'
+output_path='/home/kvshenoy/project/dataset/juan/fps0.01'
+video_metric_log='/home/kvshenoy/project/dataset/metric_log_fps0.01.csv'
+empty_frame_log='/home/kvshenoy/project/dataset/empty_frame_log_fps0.01.csv'
+with open(video_metric_log, "a") as video_metric_log_file:
+    video_metric_log_file.write("ID,FPS\n")
+with open(empty_frame_log, "a") as empty_frame_log_file:
+    empty_frame_log_file.write("ID,FRAME\n")
+
+create_folder(output_path)
+for each in classes:
+    folder_path=os.path.join(output_path,each)
+    create_folder(folder_path)
+
+time_dict=np.load('./times.npy')
+for entry in time_dict:
+    filename=entry['file']
+    filepath=os.path.join(input_dir,filename+'.mp4')
+    category=filename[:-2]
+    dest_path = os.path.join(output_path, category)
+    if category in classes: #selecting only the instrument classes with one instrument
+        extract_frames(filepath,dest_path,fps,entry['to'],entry['tf'],video_metric_log,empty_frame_log)
+    else:
+        continue
